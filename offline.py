51744ebd692e56e7e5b6f810cc4c19948d06d4b1
HongChan Yoon <hongchani0324@gmail.com>
2022-11-09 19:43:30 +0900

    update topm bottom list and extract

--- offline.py
+++ offline.py
@@ -2,27 +2,14 @@ from PIL import Image
 from pathlib import Path
 import numpy as np
 
+
 from feature_extractor import FeatureExtractor
 
+# vgg16.h5 못 받아올 때 아래 코드 2줄 활성화 후 실행
+# import ssl
+# ssl._create_default_https_context = ssl._create_unverified_context
 
-    ### 원본 코드 ###
-# if __name__ == "__main__":
-    
-#     fe = FeatureExtractor()
-    
-    # for img_path in sorted(Path("./static/img").glob("*.jpg")):
-    #     print(img_path)
-        
-    #     # Extract a deep feature here
-    #     feature = fe.extract(img=Image.open(img_path))
-        
-    #     feature_path = Path("./static/feature") / (img_path.stem + ".npy")
-    #     print(feature_path)
-        
-    #     # Save the feature
-    #     np.save(feature_path, feature)
-    ### 여기까지 ###
-        
+
 if __name__ == "__main__":
     
     fe = FeatureExtractor()
